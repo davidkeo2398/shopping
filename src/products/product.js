912ca7e08ed7acfa533e230828f4229cfe8136ae
davidkeo2398 <nguyenboiky@gmail.com>
2021-08-19 23:37:39 +0700

    change src jacket

--- src/products/product.js
+++ src/products/product.js
@@ -14,10 +14,11 @@ export default class Product extends Component {
         this.setState({ like: !this.state.like })
     }
     render() {
+        const jacket = 'https://vulcano.vn/images/products/2020/10/29/original/ao-jacket-0612-1603940407.jpg'
         return (
             <div className="wrap-content">
                 <h2 id='title'>Men Cotton Jacket</h2>
-                <img id='prodImg' src='https://vulcano.vn/images/products/2020/10/29/original/ao-jacket-0612-1603940407.jpg' />
+                <img id='prodImg' src={jacket} />
                 <h3 id='price'>55.99 $</h3>
                 <button id='btnBuy'>Buy</button>
                 <img onClick={this.handleCLick} id='heartLike' src={this.state.like ? heartSolid : heartStroke} />
