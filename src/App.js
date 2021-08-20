f667f1eab7b6a59e5ca1671fca8bb55857dc9f89
davidkeo2398 <nguyenboiky@gmail.com>
2021-08-20 18:06:56 +0700

    meow

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Product from './products/product';
 import ProductListing from './products/productListing';
-
+import cat from './assets/images/cat.jpg'
 
 function App() {
   return (
     <div className="App">
       <h1>CÀ CHỚN'S SHOP</h1>
+      <img id="cat" src={cat} ></img>
       <h4>Buy thì Buy, không Buy thì Bye!!!</h4>
       <ProductListing />
     </div>
